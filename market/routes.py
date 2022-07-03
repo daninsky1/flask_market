0a39152011c4d2a7f1578cb50a5fd4f6b8a65bab
Daniel Santos <daninsky12@gmail.com>
2022-07-03 12:25:43 -0300

    Remove old item dictionary

--- market/routes.py
+++ market/routes.py
@@ -11,11 +11,6 @@ def home_page():
 @app.route("/market")
 def market_page():
     items = Item.query.all()
-    # items = [
-    #     {"id": 1, "name": "Phone", "barcode": "123412341234", "price": 500},
-    #     {"id": 2, "name": "Laptop", "barcode": "123412341235", "price": 900},
-    #     {"id": 3, "name": "Keyboard", "barcode": "123412341236", "price": 150}
-    # ]
     return render_template("market.html", items=items)
     
 @app.route("/about")
